eb410c5f3b893b88b4a78f50a991153b5194f43c
nithinRaj031197 <nithinraj.official3@gmail.com>
2023-08-24 18:19:00 +0530

    Objects and React basic IQ

---
+++ Javascript_Interview/Javascript_Underhood/example1.js
@@ -0,0 +1,5 @@
+setTimeout(() => {
+  console.log("in");
+}, 0);
+
+console.log("out");
---
+++ Javascript_Interview/Object/object.js
@@ -0,0 +1,19 @@
+// const name = "Nithim";
+
+// const user = {
+//   name: "ex",
+//   fullName: {
+//     first: "revathi",
+//     last: "g",
+//   },
+//   age: 23,
+// };
+
+// const { name: userName } = user;
+// const {
+//   fullName: { first },
+// } = user;
+
+// console.log(first);
+
+console.log({ a: 1 } === { a: 1 });
